8741960035b637030070a7579660d871d1382785
Pese <146886140+Pesepvtltd@users.noreply.github.com>
2024-04-26 16:31:02 +0530

    Add existing file

--- program1.py
+++ program1.py
@@ -4,10 +4,22 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
-    
+        stack = []
+        bracket_map = {')': '(', ']': '[', '}': '{'}
+        
+        for char in s:
+            if char in bracket_map.values():
+                stack.append(char)
+            elif char in bracket_map:
+                if not stack or bracket_map[char] != stack.pop():
+                    return False
+            else:
+                return False
+        
+        return not stack
 
-
-
-  
-
+# Example usage:
+solution = Solution()
+print(solution.isValid("()"))        # Output: true
+print(solution.isValid("()[]{}"))    # Output: true
+print(solution.isValid("(]"))        # Output: false
--- program2.py
+++ program2.py
@@ -4,4 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        prev_value = 0
+        
+        for char in s:
+            current_value = roman_dict[char]
+            if current_value > prev_value:
+                result += current_value - 2 * prev_value
+            else:
+                result += current_value
+            prev_value = current_value
+        
+        return result
